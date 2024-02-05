3864a42aa5aa3ceb291bad4f119ca5b5cd10d734
lordwolfyyy <155068815+lordwolfyyy@users.noreply.github.com>
2024-02-05 15:56:27 +0000

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -13,12 +13,10 @@ export const authOptions: AuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
-      callbackUrl: "http://45.11.229.20:3001/api/auth/callback/github"
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-callbackUrl: "http://45.11.229.20:3001/api/auth/callback/google"
     }),
     CredentialsProvider({
       name: 'credentials',
